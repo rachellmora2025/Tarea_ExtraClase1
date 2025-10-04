7e173c69b85f23c585b801f94110f1e4948f4a9c
Rachell Mora Reyes <Rachell Mora Reyes@DESKTOP-63OHV96>
2025-10-03 18:31:13 -0600

    Arreglo conflicto 2

--- src/Extra_Clase.java
+++ src/Extra_Clase.java
@@ -12,28 +12,55 @@ import java.util.Scanner;
 
 public class Extra_Clase {
 
+    static Scanner sistema = new Scanner(System.in);
+    static ArrayList<String> nombres = new ArrayList<>();
+    static ArrayList<String> apellidos = new ArrayList<>();
+    static ArrayList<String> emails = new ArrayList<>();
+    static ArrayList<Integer> telefonos = new ArrayList<>();
+    static ArrayList<String> direcciones = new ArrayList<>();
+
     static ArrayList<String> datosCliente = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        Scanner sistema = new Scanner(System.in);
+        System.out.println("===BIENVENIDO AL SISTEMA===");
+        System.out.println("INGRESE LA CANTIDAD DE USUARIOS A REGISTRAR");
+        int cant = sistema.nextInt();
+        sistema.nextLine();
+        for (int i = 0; i < cant; i++) {
+            System.out.println("=== REGISTRO " + (i + 1) + " ===");
 
-        String nuevocliente;
-        System.out.println("Ingrese los datos del cliente a inscribir: (Nombre, Apellido, email, telefomo, direccion)  .");
-        for (int j = 0; j < 5; j++) {
-            nuevocliente = sistema.next();
+            System.out.print("Nombre: ");
+            String nombre = sistema.nextLine();
+
+            System.out.print("Apellido: ");
+            String apellido = sistema.nextLine();
+
+            System.out.print("Email: ");
+            String email = sistema.nextLine();
+
+            System.out.print("Telefono: ");
+            int telefono = sistema.nextInt();
             sistema.nextLine();
-            datosCliente.add(nuevocliente);
+            System.out.print("Direccion: ");
+            String direccion = sistema.nextLine();
 
-        }//fin for.
-        System.out.println("Se registro a " + datosCliente.get(0) + " exitosamente.");
-        
-            //comentario de prueba
-       
-            //comentario 2
+            nombres.add(nombre);
+            apellidos.add(apellido);
+            emails.add(email);
+            telefonos.add(telefono);
+            direcciones.add(direccion);
 
-        System.out.println("Se registro a " + datosCliente + " exitosamente.");
+        }
+        System.out.println("====RESUMEN DEL REGISTRO===");
+        for (int i = 0; i < cant; i++) {
+            System.out.println("===Usuario " + (i + 1) + "===");
+            System.out.println("Nombre: " + nombres.get(i) + " " + apellidos.get(i));
+            System.out.println("Email: " + emails.get(i));
+            System.out.println("Telefono: " + telefonos.get(i));
+            System.out.println("Direccion: " + direcciones.get(i));
+        }
     }//fin de main
 
-}// fin de codigo
+}// fin de codigo 
 
